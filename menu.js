296ff4abca3efb0147c54c71f037a62ee05c20de
Connor Hutnell <connorhutnell@yahoo.co.uk>
2023-05-23 15:28:03 +0100

    Created constants for the sections to insert menu items into and constant arrays for the items names Also created foreach loops to put code in to create items

---
+++ menu.js
@@ -0,0 +1,30 @@
+const pizzaSection = document.getElementById("pizza")
+const veganSection = document.getElementById("vegan")
+const glutenSection = document.getElementById("glueten-free")
+const sidesSection = document.getElementById("sides")
+const drinksSection = document.getElementById("drinks")
+const pizzaArray = ["original", "pepperoni", "meat", "chicken", "bbq", "spicy", "vegi", "spicy-vegi", "hawaiian", "tuna"]
+const veganArray = ["vegan-original", "vegan-pepperoni", "vegan-vegi"]
+const glutenArray = ["gf-original", "gf-pepperoni", "gf-vegi"]
+const sidesArray = ["garlic-bread", "cheesy-garlic-bread", "chicken-wings", "salad", "coleslaw"]
+const drinksArray = ["coke", "diet-coke", "dr-pepper", "fanta", "sprite"]
+
+pizzaArray.forEach(function(pizzaItem, index){
+   
+})
+
+veganArray.forEach(function(veganItem, index){
+    
+})
+
+glutenArray.forEach(function(glutenItem, index){
+    
+})
+
+sidesArray.forEach(function(sideItem, index){
+    
+})
+
+drinksArray.forEach(function(drinkItem, index){
+    
+})
