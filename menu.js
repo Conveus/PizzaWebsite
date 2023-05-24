2767861542529c1bb61b745c6f321d8cac8c68f1
Connor Hutnell <connorhutnell@yahoo.co.uk>
2023-05-24 22:01:40 +0100

    Started implementing the same code for the sides section as is implemented for the pizza section

--- menu.js
+++ menu.js
@@ -5,14 +5,14 @@ import data from './items.json' assert {type: 'json'}
 const pizzaSection = document.getElementById("pizza")
 const veganSection = document.getElementById("vegan")
 const glutenSection = document.getElementById("glueten-free")
-const sidesSection = document.getElementById("sides")
-const drinksSection = document.getElementById("drinks")
+const sideSection = document.getElementById("sides")
+const drinkSection = document.getElementById("drinks")
 // Setting constant arrays for menu options
-const pizzaArray = data.pizzas//["original", "pepperoni", "meat", "chicken", "bbq", "spicy", "vegi", "spicy-vegi", "hawaiian", "tuna"]
-const veganArray = data.vegans//["vegan-original", "vegan-pepperoni", "vegan-vegi"]
-const glutenArray = data.gfree//["gf-original", "gf-pepperoni", "gf-vegi"]
-const sidesArray = data.sides//["garlic-bread", "cheesy-garlic-bread", "chicken-wings", "salad", "coleslaw"]
-const drinksArray = data.drinks//["coke", "diet-coke", "dr-pepper", "fanta", "sprite"]
+const pizzaArray = data.pizzas
+const veganArray = data.vegans
+const glutenArray = data.gfree
+const sideArray = data.sides
+const drinkArray = data.drinks
 
 pizzaArray.forEach(function(pizzaItem, index){
 
@@ -21,6 +21,7 @@ pizzaArray.forEach(function(pizzaItem, index){
    pizzaPanel.className = "item"
    let pizzaImage = document.createElement("img")
    pizzaImage.src = pizzaArray[index].image
+   pizzaImage.alt = "Image"
    pizzaPanel.appendChild(pizzaImage)
 
    // First column, Title and Price
@@ -57,12 +58,12 @@ pizzaArray.forEach(function(pizzaItem, index){
    pizzaSize.name = "size"
    pizzaSize.id = "size"
    pizzaArray[index].sizes.forEach(function(size, index){
-    let sizeOp = document.createElement("option")
-    sizeOp.value = index
-    sizeOp.text = size
-    if(index==1){sizeOp.selected=true}
-    pizzaSize.add(sizeOp)
-})
+        let sizeOp = document.createElement("option")
+        sizeOp.value = index
+        sizeOp.text = size
+        if(index==1){sizeOp.selected=true}
+        pizzaSize.add(sizeOp)
+    })
    pizzaCol2.appendChild(pizzaSize)
    pizzaPanel.appendChild(pizzaCol2)
 
@@ -75,15 +76,15 @@ pizzaArray.forEach(function(pizzaItem, index){
    pizzaQuantity.name = "quantity"
    pizzaQuantity.id = "quantity"
    for (let i = 1; i<=10; i++) {
-    let quantityOp = document.createElement("option")
-    quantityOp.value = i
-    quantityOp.text = i
-    pizzaQuantity.add(quantityOp,null)
+        let quantityOp = document.createElement("option")
+        quantityOp.value = i
+        quantityOp.text = i
+        pizzaQuantity.add(quantityOp,null)
    }
    pizzaPanel.appendChild(pizzaQuantity)
    let pizzaButton = document.createElement("button")
    pizzaButton.textContent = "Add to Cart"
-   pizzaButton.dataset.order = pizzaItem
+   pizzaButton.dataset.order = "pizza"
    pizzaPanel.appendChild(pizzaButton)
 
    // Attaches panel to section
@@ -99,10 +100,53 @@ glutenArray.forEach(function(glutenItem, index){
     
 })
 
-sidesArray.forEach(function(sideItem, index){
-    
+sideArray.forEach(function(sideItem, index){
+
+   // Creates item panel and image
+   let pizzaPanel = document.createElement("div")
+   pizzaPanel.className = "item"
+   let pizzaImage = document.createElement("img")
+   pizzaImage.src = sideArray[index].image
+   pizzaPanel.appendChild(pizzaImage)
+
+   // First column, Title and Price
+   let pizzaCol1 = document.createElement("div")
+   pizzaCol1.className = "columns"
+   let pizzaTitle = document.createElement("div")
+   pizzaTitle.className = "title"
+   pizzaTitle.textContent = sideArray[index].name
+   pizzaCol1.appendChild(pizzaTitle)
+   let pizzaPrice = document.createElement("div")
+   pizzaPrice.className = "price"
+   pizzaPrice.textContent = sideArray[index].price
+   pizzaCol1.appendChild(pizzaPrice)
+   pizzaPanel.appendChild(pizzaCol1)
+
+   // Description, Quantity, and Button
+   let pizzaDesc = document.createElement("p")
+   pizzaDesc.className = "desc"
+   pizzaDesc.textContent = sideArray[index].desc
+   pizzaPanel.appendChild(pizzaDesc)
+   let pizzaQuantity = document.createElement("select")
+   pizzaQuantity.name = "quantity"
+   pizzaQuantity.id = "quantity"
+   for (let i = 1; i<=10; i++) {
+        let quantityOp = document.createElement("option")
+        quantityOp.value = i
+        quantityOp.text = i
+        pizzaQuantity.add(quantityOp,null)
+   }
+   pizzaPanel.appendChild(pizzaQuantity)
+   let pizzaButton = document.createElement("button")
+   pizzaButton.textContent = "Add to Cart"
+   pizzaButton.dataset.order = "side"
+   pizzaPanel.appendChild(pizzaButton)
+
+   // Attaches panel to section
+   sideSection.appendChild(pizzaPanel)
+
 })
 
-drinksArray.forEach(function(drinkItem, index){
+drinkArray.forEach(function(drinkItem, index){
     
 })
