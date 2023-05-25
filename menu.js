46ac1947e383448b772c573958c0ba0b9aaf6adc
Connor Hutnell <connorhutnell@yahoo.co.uk>
2023-05-25 12:14:52 +0100

    Added code to generate vegan & gluten free options Also seperated pizza generation code into a seperate file to prevent repetition

--- menu.js
+++ menu.js
@@ -1,10 +1,11 @@
 // Getting json file
 import data from './items.json' assert {type: 'json'}
+import { PizzaMaker } from './pizzaClass.js' //assert {type: 'javascript'}
 
 // Setting constants for sections
 const pizzaSection = document.getElementById("pizza")
 const veganSection = document.getElementById("vegan")
-const glutenSection = document.getElementById("glueten-free")
+const glutenSection = document.getElementById("gluten-free")
 const sideSection = document.getElementById("sides")
 const drinkSection = document.getElementById("drinks")
 // Setting constant arrays for menu options
@@ -15,89 +16,18 @@ const sideArray = data.sides
 const drinkArray = data.drinks
 
 pizzaArray.forEach(function(pizzaItem, index){
-
-    // Creates item panel and image
-   let itemPanel = document.createElement("div")
-   itemPanel.className = "item"
-   let itemImage = document.createElement("img")
-   itemImage.src = pizzaArray[index].image
-   itemImage.alt = "Image"
-   itemPanel.appendChild(itemImage)
-
-   // First column, Title and Price
-   let itemCol1 = document.createElement("div")
-   itemCol1.className = "columns"
-   let itemTitle = document.createElement("div")
-   itemTitle.className = "title"
-   itemTitle.textContent = pizzaArray[index].name
-   itemCol1.appendChild(itemTitle)
-   let itemPrice = document.createElement("div")
-   itemPrice.className = "price"
-   itemPrice.textContent = pizzaArray[index].price
-   itemCol1.appendChild(itemPrice)
-   itemPanel.appendChild(itemCol1)
-
-   // Second column
-   let itemCol2 = document.createElement("div")
-   itemCol2.className = "columns"
-   // Base options
-   let pizzaBase = document.createElement("select")
-   pizzaBase.className = "base"
-   pizzaBase.name = "base"
-   pizzaBase.id = "base"
-   pizzaArray[index].bases.forEach(function(base, index){
-        let baseOp = document.createElement("option")
-        baseOp.value = index
-        baseOp.text = base
-        pizzaBase.add(baseOp)
-   })
-   itemCol2.appendChild(pizzaBase)
-   // Size options
-   let pizzaSize = document.createElement("select")
-   pizzaSize.className = "size"
-   pizzaSize.name = "size"
-   pizzaSize.id = "size"
-   pizzaArray[index].sizes.forEach(function(size, index){
-        let sizeOp = document.createElement("option")
-        sizeOp.value = index
-        sizeOp.text = size
-        if(index==1){sizeOp.selected=true}
-        pizzaSize.add(sizeOp)
-    })
-   itemCol2.appendChild(pizzaSize)
-   itemPanel.appendChild(itemCol2)
-
-   // Description, Quantity, and Button
-   let itemDesc = document.createElement("p")
-   itemDesc.className = "desc"
-   itemDesc.textContent = pizzaArray[index].desc
-   itemPanel.appendChild(itemDesc)
-   let itemQuantity = document.createElement("select")
-   itemQuantity.name = "quantity"
-   itemQuantity.id = "quantity"
-   for (let i = 1; i<=10; i++) {
-        let quantityOp = document.createElement("option")
-        quantityOp.value = i
-        quantityOp.text = i
-        itemQuantity.add(quantityOp,null)
-   }
-   itemPanel.appendChild(itemQuantity)
-   let itemButton = document.createElement("button")
-   itemButton.textContent = "Add to Cart"
-   itemButton.dataset.order = "pizza"
-   itemPanel.appendChild(itemButton)
-
-   // Attaches panel to section
-   pizzaSection.appendChild(itemPanel)
-   
+     let pizzaOp = new PizzaMaker()
+     pizzaOp.addPizza(pizzaSection, pizzaItem)
 })
 
 veganArray.forEach(function(veganItem, index){
-    
+     let veganOp = new PizzaMaker()
+     veganOp.addPizza(veganSection, veganItem)
 })
 
 glutenArray.forEach(function(glutenItem, index){
-    
+     let glutenOp = new PizzaMaker()
+     glutenOp.addPizza(glutenSection, glutenItem)
 })
 
 sideArray.forEach(function(sideItem, index){
---
+++ pizzaClass.js
@@ -0,0 +1,79 @@
+export class PizzaMaker{
+    constructor(){}
+
+    addPizza(location, pizzaOb){
+        // Creates item panel and image
+        let itemPanel = document.createElement("div")
+        itemPanel.className = "item"
+        let itemImage = document.createElement("img")
+        itemImage.src = pizzaOb.image
+        itemImage.alt = "Image"
+        itemPanel.appendChild(itemImage)
+     
+        // First column, Title and Price
+        let itemCol1 = document.createElement("div")
+        itemCol1.className = "columns"
+        let itemTitle = document.createElement("div")
+        itemTitle.className = "title"
+        itemTitle.textContent = pizzaOb.name
+        itemCol1.appendChild(itemTitle)
+        let itemPrice = document.createElement("div")
+        itemPrice.className = "price"
+        itemPrice.textContent = pizzaOb.price
+        itemCol1.appendChild(itemPrice)
+        itemPanel.appendChild(itemCol1)
+     
+        // Second column
+        let itemCol2 = document.createElement("div")
+        itemCol2.className = "columns"
+        // Base options
+        let pizzaBase = document.createElement("select")
+        pizzaBase.className = "base"
+        pizzaBase.name = "base"
+        pizzaBase.id = "base"
+        pizzaOb.bases.forEach(function(base, index){
+             let baseOp = document.createElement("option")
+             baseOp.value = index
+             baseOp.text = base
+             pizzaBase.add(baseOp)
+        })
+        itemCol2.appendChild(pizzaBase)
+        // Size options
+        let pizzaSize = document.createElement("select")
+        pizzaSize.className = "size"
+        pizzaSize.name = "size"
+        pizzaSize.id = "size"
+        pizzaOb.sizes.forEach(function(size, index){
+             let sizeOp = document.createElement("option")
+             sizeOp.value = index
+             sizeOp.text = size
+             if(index==1){sizeOp.selected=true}
+             pizzaSize.add(sizeOp)
+         })
+        itemCol2.appendChild(pizzaSize)
+        itemPanel.appendChild(itemCol2)
+     
+        // Description, Quantity, and Button
+        let itemDesc = document.createElement("p")
+        itemDesc.className = "desc"
+        itemDesc.textContent = pizzaOb.desc
+        itemPanel.appendChild(itemDesc)
+        let itemQuantity = document.createElement("select")
+        itemQuantity.name = "quantity"
+        itemQuantity.id = "quantity"
+        for (let i = 1; i<=10; i++) {
+             let quantityOp = document.createElement("option")
+             quantityOp.value = i
+             quantityOp.text = i
+             itemQuantity.add(quantityOp,null)
+        }
+        itemPanel.appendChild(itemQuantity)
+        let itemButton = document.createElement("button")
+        itemButton.textContent = "Add to Cart"
+        itemButton.dataset.order = "pizza"
+        itemPanel.appendChild(itemButton)
+     
+        // Attaches panel to section
+        location.appendChild(itemPanel)
+    }
+}
